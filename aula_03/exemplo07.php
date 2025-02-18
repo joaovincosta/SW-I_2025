ca6d4fb32fe43c8f13d363137911957de7dbc849
João Vinícius Costa <161096439+joaovincosta@users.noreply.github.com>
2025-02-18 12:34:30 -0300

    Add files via upload

---
+++ aula_03/exemplo07.php
@@ -0,0 +1,14 @@
+<?php
+
+    $a = 10;
+    $b = "10";
+
+    $teste1 = ($a == $b);
+    echo "Teste 1 ==> $teste1";
+
+    echo "<br>";
+
+    $teste2 = ($a === $b);
+    echo "Teste 2 ==> $teste2";
+
+?>
