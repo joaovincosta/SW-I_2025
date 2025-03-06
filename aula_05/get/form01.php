7d8567f7d12fdf62756821db584d75b3705ce68c
João Vinícius Costa <161096439+joaovincosta@users.noreply.github.com>
2025-03-06 09:07:44 -0300

    Add files via upload

--- aula_05/get/form01.php
+++ aula_05/get/form01.php
@@ -1,3 +1,11 @@
 <?php 
 
+    $nota01 = $_GET['nota1'];
+    $nota02 = $_GET['nota2'];
+    $nota03 = $_GET['nota3'];
+
+    $media = ($nota01 + $nota02 + $nota03) / 3;
+
+    echo "A média de suas três notas é: $media";
+    
 ?>
--- aula_05/get/form02.php
+++ aula_05/get/form02.php
@@ -1,3 +1,10 @@
 <?php 
 
+    $num01 = $_GET['num1'];
+    $num02 = $_GET['num2'];
+    $num03 = $_GET['num3'];
+
+    if ($num01 > $num02 && $num01 > $num03):
+        echo "O primeiro número, $num01, é o maior";
+    endif;
 ?>
