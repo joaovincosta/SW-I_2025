0fa8de911d5e1f189a2795727dc2bc0eea56bb7d
João Vinícius Costa <161096439+joaovincosta@users.noreply.github.com>
2025-03-10 08:54:52 -0300

    Add files via upload

--- aula_05/post/form03.php
+++ aula_05/post/form03.php
@@ -1,3 +1,16 @@
 <?php 
 
+    $cor=$_POST['color'];
+       
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+    <body style="background-color: <?php echo "$cor" ?>";
+</body>
+</html>
