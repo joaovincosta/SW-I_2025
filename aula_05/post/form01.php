72b7e2fed23f8f1415c4040367bc865becb61a0c
João Vinícius Costa <161096439+joaovincosta@users.noreply.github.com>
2025-03-10 08:18:35 -0300

    Add files via upload

--- aula_05/post/form01.php
+++ aula_05/post/form01.php
@@ -1,3 +1,12 @@
 <?php 
 
+    $usuario=$_POST['user'];
+    $senha=$_POST['key'];
+
+    if ($usuario == "etec" && $senha == "informática"):
+        echo "Logado com sucesso!";
+    else:
+        echo "Usuário ou senha incorretos. Tente novamente";
+    endif;
+
 ?>
