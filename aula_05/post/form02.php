d2354f51538b9cefa9b309d7a31b4d458acb1646
João Vinícius Costa <161096439+joaovincosta@users.noreply.github.com>
2025-03-10 08:40:29 -0300

    Add files via upload

--- aula_05/post/form02.php
+++ aula_05/post/form02.php
@@ -1,3 +1,10 @@
-<?php 
+<?php
+
+    $nome=$_POST['name'];
+    $email=$_POST['e-mail'];
+    $nascimento=$_POST['date'];
+    $bandeira_cartao=$_POST['cartao'];
+
+    echo "Ok: $nome, nascido em $nascimento, cujo endereço de e-mail é $email e a bandeira do cartão é $bandeira_cartao. Obrigado!"
 
 ?>
