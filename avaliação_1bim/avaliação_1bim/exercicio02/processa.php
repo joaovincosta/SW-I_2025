6c7bce9ed1b4e0f0bfcf07ca372b7fb08acc047f
João Vinícius Costa <161096439+joaovincosta@users.noreply.github.com>
2025-04-15 03:07:55 -0300

    Delete avaliação_1bim/avaliação_1bim directory

--- avaliação_1bim/avaliação_1bim/exercicio02/processa.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $celsius = $_GET['tempcelsius'];
-
-    $fahrenheit = ($celsius * 9/5) + 32;
-    $kelvin = $celsius + 273.15;
-
-    echo "Em Fahrenheit, essa temperatura fica $fahrenheit graus<br>";
-    echo "Em Kelvin, essa temperatura fica $kelvin";
-?>
