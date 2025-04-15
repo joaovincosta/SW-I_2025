b5f4ceb0446fb6847606d154310a3c05e34a2eee
João Vinícius Costa <161096439+joaovincosta@users.noreply.github.com>
2025-04-15 03:43:48 -0300

    Add files via upload

---
+++ avaliação_1bim/exercicio04/exercicio04.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício 04 - Avaliação de SW1 do 1º Bimestre</title>
+</head>
+<body>
+    <h2>Olá!</h2>
+    <?php
+        $multiplode3 = [];
+
+        for ($i = 3; $i <= 100; $i += 3) {
+            $multiplode3[] = $i; 
+        }
+
+        $soma = 0;
+        foreach ($multiplode3 as $num) {
+            $soma += $num;
+        }
+
+        echo "A soma de todos os números de 1 até 100 que são múltiplos de 3 é: $soma";
+    ?>
+</body>
+</html>
